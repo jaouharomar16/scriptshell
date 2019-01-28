6a171d719473fef40988ac6242954334a01913a0
jaouharomar16 <omar.jaouhar@e-polytechnique.ma>
2019-01-28 15:34:10 +0100

    ajouter des commentaires pour rendre le script lisible

--- somme.sh
+++ somme.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 res = 0
 
-
+#condition sur le nombre de parametre d'entreé
 if [ $# -eq 3 ]
 	then 
-		
+		#Addition
 		if [ $2 = '+' ]
 		then $res=$(($1 + $3))
     	echo "$res"
+    	#soustraction
     	elif [ $2 = '-' ]
 		then $res=$(($1 - $3))
 		echo "$res"
+		#division
 		elif [ $2 = '/' ]
 		then $res=$(($1 / $3))
 		echo "$res"
+		#multiplication
 		elif [ $2 = '*' ]
 		then $res=$(($1 * $3))
 		echo "$res"
